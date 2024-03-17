d5166b390c8c2719db6be5c52915d27abbe3d6d7
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-17 15:44:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+   const cleandWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+   const reverse = cleandWord.split('').reverse().join('');
+
+   return cleandWord === reverse;
+    
+ 
+
 }
+
+const result1 = isPalindrome("lalal");
+console.log(result1); // Output: true
+
+const result2 = isPalindrome("hello");
+console.log(result2); // Output: false
 
 /* 
   Add your pseudocode here
+  .iterate over on the given word
+  .check if it's can read from beginning and from ending 
+    .false if it is not 
+  .return that word
 */
 
 /*
